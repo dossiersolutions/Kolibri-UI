f86d488e2048116a592c5636745bffbfac7fe614
Jostein Berre Eliassen <spam@jostein.be>
2017-10-24 13:02:23 +0200

    Updates

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "b98ce0fb133fa29b5644"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "f2d3e4ce826e2d7da0df"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -707,7 +707,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/kolibri-ui";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
